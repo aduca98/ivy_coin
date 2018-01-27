105eefe5afbf05ce6127a1eaac11eb5f572650ed
Menoikeus <datonnguyen@gmail.com>
2018-01-27 17:58:16 -0500

    We now have a half and half map screen

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -32,138 +32,34 @@ class Home extends React.Component {
         })
     }
     async componentWillMount() {
-        
+
     }
     async fetchBalance() {
-        
+
     }
-   
+
     render() {
-      
+
         const {navigation} = this.props;
         //
         // if(this.state.loading) {
         //     return <Loading />
         // }
-        return (<MapView 
-                    style={{ 
-                        flex: 1,
-                        flexDirection: 'column',
-                        justifyContent: 'flex-end',
-                    }}
-                    >
-                   
-                    {/*<StatusBar
-                        barStyle="light-content"
-                    />*/}
-                    {/*<View style={{
-                        height: 100,
-                        padding:0,
-                        top: -10,
-                        paddingTop: 20,
-                        left:0,
-                        backgroundColor: 'red',
-                        width: WindowDimensions.width }}>
-
-                        <View style={{
-                                flex: .25,
-                                flexDirection: 'row',
-                            }}>
-                            <Left style={{
-                                    left: 0,
-                                    flex: .25,
-                                    top: 35,
-                                    justifyContent: 'flex-start'
-                                }}>
-                                <Button onPress={() => navigation.navigate("DrawerOpen")} transparent>
-                                    <Image style={{
-                                        width: 35,
-                                        height: 25,
-                                        left: 15
-                                    }} source={require('../../assets/menu.png')} />
-                                    
-                                </Button>
-                            </Left>
-                            
-                            <Text
-                                style={{
-                                    backgroundColor: 'transparent',
-                                    fontSize: 25,
-                                    color: '#fff',
-                                    flex: .5,
-                                    height: 50,
-                                    textAlign: 'center',
-                                    top: 30,
-                                    justifyContent: 'flex-start',
-                                    fontFamily: 'Avenir-Book'
-                                }}>
-                                Home 
-                            </Text>
-
-                            <View style={{flex: .25}}></View>
-
-                        </View>
-
-                    </View>*/}
-                    <View style={{
-                        width: WindowDimensions.width,
-                        height: 400,
-                        bottom: 0,
-                        alignSelf: 'flex-end',
-                        backgroundColor: '#efefef'
-                    }}>
-                        <ScrollView
-                            horizontal={true}>
-
-                            <View style={{
-                                backgroundColor: 'red',
-                                width: 150,
-                                height: 150,
-                                marginRight: 30,
-                            }}>
-                            </View>
-
-                            <View style={{
-                                backgroundColor: 'red',
-                                width: 150,
-                                height: 150,
-                                marginRight: 30,
-                            }}>
-                            </View>
-                            <View style={{
-                                backgroundColor: 'red',
-                                width: 150,
-                                height: 150,
-                                marginRight: 30,
-                            }}>
-                            </View>
-                            <View style={{
-                                backgroundColor: 'red',
-                                width: 150,
-                                height: 150,
-                            }}>
-                            </View>
-
-                        </ScrollView>
-
-                        <TouchableOpacity 
-                            onPress={() => this.props.navigation.navigate("Food_Display")}>
-                            <Text>See Food</Text>
-                            
-                        </TouchableOpacity>
-                        
-                        <TouchableOpacity 
-                            onPress={() => this.props.navigation.navigate("Deliveries_Display")}>
-                            <Text>Deliver Food</Text>
-                        </TouchableOpacity>
-                        
-                    </View>
-                        
-                </MapView>);
+        return (
+          <View style={{
+            flex: 1,
+            flexDirection: 'column'
+          }}>
+            <MapView style={{
+                flex: 1
+            }}>
+            </MapView>
+            <Text style={{ flex: 1 }}> OI </Text>
+          </View>);
     }
 }
 function cacheImages(images) {
-    return images.map(image => 
+    return images.map(image =>
     Expo.Asset.fromModule(image).downloadAsync());
 }
 
@@ -185,11 +81,11 @@ const style = StyleSheet.create({
     }
 });
 
-function mapStateToProps(state) { 
+function mapStateToProps(state) {
     return {
-        
-    }; 
-} 
+
+    };
+}
 
 function mapDispatchToProps(dispatch) {
     return {
@@ -199,7 +95,7 @@ function mapDispatchToProps(dispatch) {
     }
 }
 
-export default connect( 
-    mapStateToProps, 
-    mapDispatchToProps 
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
 )(Home);
