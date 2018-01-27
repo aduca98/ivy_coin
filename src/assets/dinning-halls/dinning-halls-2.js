29138fe71ae07903b84a87de33de603176b17da2
DESKTOP-2JQ2EH9\Evan Velasquez <evanmv13@hotmail.com>
2018-01-27 15:16:13 -0500

    dininghalls2 and images

--- src/assets/dinning-halls/dinning-halls-2.js
+++ src/assets/dinning-halls/dinning-halls-2.js
@@ -1,24 +1,250 @@
 export default {
     dinningHalls: {
-        1: {
+        4: {
             name: "Ratty",
             description: "whatever...",
-            photo: require('./images/hello.jpg'),
+            photo: require('./images/Ratty.jpg'),
             location: {
-                address: "dasdasd",
-                lat: 760.00,
-                long: 775.00
+                address: "Upper Level, 144 Thayer St, Providence, RI 02906",
+                lat: 41.825218,
+                long: -71.401232
             },
             food: [
                 {
-                    name: "",
+                    name: "Scrambled Eggs",
+                    description: "",
+                    photo: require('./images/scrambled-eggs.jpg')
+                },
+                {
+                    name: "Pancakes",
+                    description: "",
+                    photo: require('./images/pancake.png')
+                },
+                {
+                    name: "Breakfast Sausage",
+                    description: "",
+                    photo: require('./images/breakfast-sausage.png')
+                },
+                {
+                    name: "Home Fries with Garlic",
+                    description: "",
+                    photo: require('./images/home-fries.jpg')
+                },
+                {
+                    name: "Bacon",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Whole Wheat Penne Pasta",
                     description: "",
                     photo: require('./images/whatever-file-name-is.png')
                 },
-                {...}
+                {
+                    name: "Red Potato Frittata",
+                    description: "",
+                    photo: require('./images/fritata.jpg')
+                },
+                {
+                    name: "Tamale Pie",
+                    description: "",
+                    photo: require('./images/tamale-pie-3-550.jpg')
+                },
+                {
+                    name: "Veggie Pizza",
+                    description: "",
+                    photo: require('./images/veg-pizza.jpg')
+                },
+                {
+                    name: "Pepperoni Pizza",
+                    description: "",
+                    photo: require('./images/pepperoni-pizza.jpg')
+                },
+                {
+                    name: "Grilled Itallian Chicken",
+                    description: "",
+                    photo: require('./images/itallian-chicken.jpg')
+                },
+                {
+                    name: "Fried Southwest Black Bean Burger",
+                    description: "",
+                    photo: require('./images/black-bean-burger.jpg')
+                },
+                {
+                    name: "Curly Fries",
+                    description: "",
+                    photo: require('./images/curly-fries.jpg')
+                },
+                {
+                    name: "Waffle Fries",
+                    description: "",
+                    photo: require('./images/waffle-fries.jpg')
+                },
+                {
+                    name: "m&m Cookies",
+                    description: "",
+                    photo: require('./images/mm-cookies.jpg')
+                },
+                {
+                    name: "S'mores Cupcakes",
+                    description: "",
+                    photo: require('./images/smores-cupcake.jpg')
+                }
             ]
         },
-        2: { ... },
-        3: { ... }
+        5: {
+            name: "Jo's",
+            description: "whatever...",
+            photo: require('./images/jos.jpg'),
+            location: {
+                address: "114 Power St, Providence, RI 02912",
+                lat: 41.823639, 
+                long: -71.399550
+            },
+            food: [
+                {
+                    name: "Spicy With",
+                    description: "",
+                    photo: require('./images/spicy-with-cmon-we-can-find-a-better-picture-than-this.jpg')
+                },
+                {
+                    name: "Waffle Fries",
+                    description: "",
+                    photo: require('./images/waffle-fries.jpg')
+                },
+                {
+                    name: "Mac and Cheese Bacon Burger",
+                    description: "",
+                    photo: require('./images/mac-n-cheese-bacon-burger.jpg')
+                },
+                {
+                    name: "Cobb Salad",
+                    description: "",
+                    photo: require('./images/cobb-salad.jpg')
+                },
+                {
+                    name: "Caesar Salad",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Burger",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Beef Tacos (x3)",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Chicken Tacos (x3)",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Omelet Bowl",
+                    description: "",
+                    photo: require('./images/')
+                }
+            ]
+        },
+        6: {
+            name: "Ivy Room",
+            description: "whatever...",
+            photo: require('./images/need-a-picture.jpg'),
+            location: {
+                address: "Lower Level, 144 Thayer St, Providence, RI 02906",
+                lat: 41.825218,
+                long: -71.401232
+            },
+            food: [
+                {
+                    name: "Veggie Tacos",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Avocado Toast",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Strawberry Smoothie",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Mango Smoothie",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Custom Smoothie",
+                    description: "don't forget to add a note to this order!",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Mashed Potatoes",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Chicken and Dumplings",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Curried Rice",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Jamaican Beef Patties",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "White and Milk Chocolate Chip Cookies",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Split Pea Soup",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Squash Bisque",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Veggie Pakora",
+                    description: "",
+                   photo: require('./images/')
+                },
+                {
+                    name: "Cocktail Vegetable Samosa",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Curly Fries",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Alu Gobhi",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Custom Falafel",
+                    description: "don't forget to add a note to this order!",
+                    photo: require('./images/')
+                }
+            ]
+        },
     }
 }
