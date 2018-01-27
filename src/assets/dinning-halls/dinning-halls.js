c62f39e0ce9e2e8df27eddb0d1db81391f6b7404
DESKTOP-8QDV8MB\georg <george_lee@brown.edu>
2018-01-27 15:26:55 -0500

    no message

--- src/assets/dinning-halls/dinning-halls.js
+++ src/assets/dinning-halls/dinning-halls.js
@@ -1,9 +1,84 @@
 export default {
     dinningHalls: {
         1: {
-            name: "Ratty",
-            description: "whatever...",
-            photo: require('./images/hello.jpg'),
+            name: "Andrews",
+            description: "This popular dinning hall contains classics such as pizzas and sandwiches as well as more exotic options such as pho and curry. ",
+            photo: require('./images/Andrews.jpg'),
+            location: {
+                address: "211 Bowen St, Providence, RI 02912",
+                lat: 41.830583,
+                long:   -71.402411
+            },
+            food: [
+                {
+                    name: "Cheese Pizza",
+                    description: "fresh dough from our bakeshop, house-made sauce, and a blend of cheeses, hearth-baked for a perfect slice",
+                    photo: require('./images/cheesePizza.jpg')
+                },
+                {
+                    name: "Pepperoni Pizza",
+                    description: "spicy pepperoni slices on our hand-made cheese pizza",
+                    photo: require('./images/whatever-file-name-is.png')
+                },
+                {
+                    name: "Pepperoni & Meatball",
+                    description: "classic peperoni with house made meatballs",
+                    photo: require('./images/pepPizza.jpg')
+                },
+                {
+                    name: "Cookie Slice",
+                    description: "giant chocolate chip cookie, baked in a pizza pan. served warm :)",
+                    photo: require('./images/cookiePizza.jpg')
+                },
+                {
+                    name: "Brownie",
+                    description: "giant brownie, baked in a pizza pan. served warm :)",
+                    photo: require('./images/BrowniePizza.jpg')
+                },
+                {
+                    name: "Bacon & Feta",
+                    description: "niman ranch bacon & narragansett creamery feta, house made pizza sauce",
+                    photo: require('./images/bacon&Feta.jpg')
+                },
+                {
+                    name: "Brunch Burrito",
+                    description: "an andrews classic! get here early to avoid the line :)",
+                    photo: require('./images/brunchBurrito.jpg')
+                },
+                {
+                    name: "Pho",
+                    description: "Our twist on the classic Vietnamese soup. start with fresh rice noodles, cooked to order. choose from an array of vegetables, house roasted sirloin beef, all natural chicken breast or tofu. finish with your choice of chicken broth, vegetable broth or green curry broth.",
+                    photo: require('./images/pho.jpg')
+                },
+                {
+                    name: "Granola Bowl",
+                    description: "andrews famous granola, local yogurt, fresh fruit. rotating toppings available",
+                    photo: require('./images/gBowl.jpg')
+                },
+            ]
+        },
+        2: {
+            name: "VDub",
+            description: "Located on the Pembroke campus, Verney-Woolley (better known as the “V-Dub”) is the smaller of Brown’s two all-you-care-to-eat dining halls. Its intimacy and unique character carry over to its work environment where our culinary team prepares grilled items in full view of the line. Dine with friends at our round tables or booth seating, in the sunny windows of the dining room, or outside in the inner courtyard. Looking to grab a quiet bite? The V-Dub has counter seating perfect for solo diners.",
+            photo: require('./images/VDub.jpg'),
+            location: {
+                address: "135 Cushing St, Providence, RI 02912",
+                lat: 41.829802,
+                long: -71.401696
+            },
+            food: [
+                {
+                    name: "",
+                    description: "",
+                    photo: require('./images')
+                },
+
+            ]
+        },
+        3: {
+            name: "Blue Room",
+            description: "Conveniently located in the Stephen Robert Campus Center, the Blue Room Café features sustainable fair-trade coffee, delicious breakfast sandwiches and a fantastic array of freshly baked goodies and artisan pastries. The Blue Room also boasts an extensive, customizable deli with local artisan bread, high-quality meats, and plenty of vegetarian options. Locally prepared soups, sushi, and a wide selection of fresh snacks and beverages are available for folks on the go. The Blue Room also partners with local restaurants serving authentic ethnic cuisine.",
+            photo: require('./images/BlueRoom.jpg'),
             location: {
                 address: "dasdasd",
                 lat: 760.00,
@@ -11,14 +86,277 @@ export default {
             },
             food: [
                 {
-                    name: "",
+                    name: "CHICKEN, KALE & SWEET POTATO SOUP",
+                    description: "chicken, kale, sweet potato, kidney beans, vegetables in a hearty chicken broth",
+                    photo: require('./images/CKSPSoup.jpg')
+                },
+                {
+                    name: "BAKED POTATO SOUP",
+                    description: "potatoes, uncured bacon, chives, and spice in a creamy chicken broth",
+                    photo: require('./images/BPSoup.jpg')
+                },
+                {
+                    name: "Butternut Squash & Apple Soup",
+                    description: "pureed butternut squash and apples with fresh rosemary in a flavorful chicken broth with cream and butter",
+                    photo: require('./images/BSSoup.jpg')
+                },
+                {
+                    name: "MADRAS CHICKEN CURRY WITH GREEN BEANS",
+                    description: "white meat chicken, coconut milk, tamarind paste, red chili powder, coriander and cumin powder, star anise, herbs, spices and green beans",
+                    photo: require('./images/MadrasChicken.jpg')
+                },
+                {
+                    name: "VEGETABLE DHANSAK",
+                    description: "butternut squash, eggplant, carrot, green beans, cauliflower, corn, lima bans, coriander and cumin powder, tamarind paste and coconut paste",
+                    photo: require('./images/VegieDhansak.jpg')
+                },
+            ]
+        }
+        4: {
+            name: "Ratty",
+            description: "whatever...",
+            photo: require('./images/Ratty.jpg'),
+            location: {
+                address: "Upper Level, 144 Thayer St, Providence, RI 02906",
+                lat: 41.825218,
+                long: -71.401232
+            },
+            food: [
+                {
+                    name: "Scrambled Eggs",
+                    description: "",
+                    photo: require('./images/scrambled-eggs.jpg')
+                },
+                {
+                    name: "Pancakes",
+                    description: "",
+                    photo: require('./images/pancake.png')
+                },
+                {
+                    name: "Breakfast Sausage",
+                    description: "",
+                    photo: require('./images/breakfast-sausage.png')
+                },
+                {
+                    name: "Home Fries with Garlic",
+                    description: "",
+                    photo: require('./images/home-fries.jpg')
+                },
+                {
+                    name: "Bacon",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Whole Wheat Penne Pasta",
                     description: "",
                     photo: require('./images/whatever-file-name-is.png')
                 },
-                {...}
+                {
+                    name: "Red Potato Frittata",
+                    description: "",
+                    photo: require('./images/fritata.jpg')
+                },
+                {
+                    name: "Tamale Pie",
+                    description: "",
+                    photo: require('./images/tamale-pie-3-550.jpg')
+                },
+                {
+                    name: "Veggie Pizza",
+                    description: "",
+                    photo: require('./images/veg-pizza.jpg')
+                },
+                {
+                    name: "Pepperoni Pizza",
+                    description: "",
+                    photo: require('./images/pepperoni-pizza.jpg')
+                },
+                {
+                    name: "Grilled Itallian Chicken",
+                    description: "",
+                    photo: require('./images/itallian-chicken.jpg')
+                },
+                {
+                    name: "Fried Southwest Black Bean Burger",
+                    description: "",
+                    photo: require('./images/black-bean-burger.jpg')
+                },
+                {
+                    name: "Curly Fries",
+                    description: "",
+                    photo: require('./images/curly-fries.jpg')
+                },
+                {
+                    name: "Waffle Fries",
+                    description: "",
+                    photo: require('./images/waffle-fries.jpg')
+                },
+                {
+                    name: "m&m Cookies",
+                    description: "",
+                    photo: require('./images/mm-cookies.jpg')
+                },
+                {
+                    name: "S'mores Cupcakes",
+                    description: "",
+                    photo: require('./images/smores-cupcake.jpg')
+                }
             ]
         },
-        2: { ... },
-        3: { ... }
+        5: {
+            name: "Jo's",
+            description: "whatever...",
+            photo: require('./images/jos.jpg'),
+            location: {
+                address: "114 Power St, Providence, RI 02912",
+                lat: 41.823639,
+                long: -71.399550
+            },
+            food: [
+                {
+                    name: "Spicy With",
+                    description: "",
+                    photo: require('./images/spicy-with-cmon-we-can-find-a-better-picture-than-this.jpg')
+                },
+                {
+                    name: "Waffle Fries",
+                    description: "",
+                    photo: require('./images/waffle-fries.jpg')
+                },
+                {
+                    name: "Mac and Cheese Bacon Burger",
+                    description: "",
+                    photo: require('./images/mac-n-cheese-bacon-burger.jpg')
+                },
+                {
+                    name: "Cobb Salad",
+                    description: "",
+                    photo: require('./images/cobb-salad.jpg')
+                },
+                {
+                    name: "Caesar Salad",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Burger",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Beef Tacos (x3)",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Chicken Tacos (x3)",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Omelet Bowl",
+                    description: "",
+                    photo: require('./images/')
+                }
+            ]
+        },
+        6: {
+            name: "Ivy Room",
+            description: "whatever...",
+            photo: require('./images/need-a-picture.jpg'),
+            location: {
+                address: "Lower Level, 144 Thayer St, Providence, RI 02906",
+                lat: 41.825218,
+                long: -71.401232
+            },
+            food: [
+                {
+                    name: "Veggie Tacos",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Avocado Toast",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Strawberry Smoothie",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Mango Smoothie",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Custom Smoothie",
+                    description: "don't forget to add a note to this order!",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Mashed Potatoes",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Chicken and Dumplings",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Curried Rice",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Jamaican Beef Patties",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "White and Milk Chocolate Chip Cookies",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Split Pea Soup",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Squash Bisque",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Veggie Pakora",
+                    description: "",
+                   photo: require('./images/')
+                },
+                {
+                    name: "Cocktail Vegetable Samosa",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Curly Fries",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Alu Gobhi",
+                    description: "",
+                    photo: require('./images/')
+                },
+                {
+                    name: "Custom Falafel",
+                    description: "don't forget to add a note to this order!",
+                    photo: require('./images/')
+                }
+            ]
+        }
     }
 }
